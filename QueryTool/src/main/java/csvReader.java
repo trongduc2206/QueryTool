f44cb9e469e161592250270f21ccc65e55c1d9c4
Trong Duc <trongduc22062000@gmail.com>
2020-10-21 21:34:37 +0700

    first commit

---
+++ QueryTool/src/main/java/csvReader.java
@@ -0,0 +1,60 @@
+import java.io.IOException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+public class csvReader {
+    public static void main(String[] args) throws IOException {
+        Map parentId = new HashMap<String, Long>();
+        Map parentId2 = new HashMap<String, Long>();
+        String temp = "";
+        String temp2 = "";
+        String temp3 = "";
+        long cnt = -1;
+        long cnt2 = -1;
+        Reader reader = new FileReader("src/main/java/DATA_LOCATION.csv");
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
+        Iterable<CSVRecord> csvRecords = csvParser.getRecords();
+        for (CSVRecord csvRecord : csvRecords) {
+
+            if (csvRecord.get(0).equals(temp)) continue;
+            else {
+                temp = csvRecord.get(0);
+                cnt++;
+                parentId.put(csvRecord.get(2), cnt);
+            }
+            // insert city
+//            System.out.println("insert into data_location(name,code,type) values('" + csvRecord.get(0) +
+//                    "', '" + csvRecord.get(2) + "', 1);");
+        }
+        // insert district
+        for (CSVRecord csvRecord : csvRecords) {
+            if (csvRecord.get(3).equals(temp2)) continue;
+            else {
+                temp2 = csvRecord.get(3);
+                cnt++;
+                parentId2.put(csvRecord.get(5), cnt - 1);
+            }
+//            System.out.println("insert into data_location(parent_id,name,code,type) values("
+//                    + parentId.get(csvRecord.get(2)) + ", '" + csvRecord.get(3) + "', '"
+//                    + csvRecord.get(5)+"',2);");
+        }
+        for (CSVRecord csvRecord : csvRecords) {
+            if (csvRecord.get(6).equals(temp3)) continue;
+            else {
+                temp3 = csvRecord.get(6);
+                cnt++;
+            }
+            System.out.println("insert into data_location(parent_id,name,code,type) values("
+                    + parentId2.get(csvRecord.get(5)) + ",'" + csvRecord.get(6) + "', '"
+                    +csvRecord.get(8)+"' ,3);"
+            );
+        }
+        System.out.println(parentId2.get("148"));
+    }
+}
